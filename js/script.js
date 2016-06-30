800e482e7c4854a3fc26bf7fac3fe072c9f0dfea
paulolimarb <paulolimarb@gmail.com>
2016-06-30 17:56:53 -0300

    acertoslide

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
       
-	  $('.slidemarca').slick({
 		
-		slidesToShow: 10,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1000,
-		});
+		$('.slidemarca').slick({
+			centerMode: true,
+			centerPadding: '60px',
+			slidesToShow: 7,
+			autoplay: true,
+			autoplaySpeed: 1000,
+			responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 3
+      }
+    }
+  ]
+});
 
 
 
